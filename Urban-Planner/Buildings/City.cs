aab6ae755b01c1fbcba9f49355ce88015f5bef92
Martin Sisk <Brothermj@hotmail.com>
2021-08-06 17:19:45 -0500

    Created a City.cs file and Class within the Building. Decleared Name, Mayor, and Year properties at the top of the Class file. Also wrote a private List<Building> for the collection of all the Buildings in the City.

---
+++ Urban-Planner/Buildings/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Urban_Planner.Buildings
+{
+    class City
+    {
+
+        public string Name { get; set; }
+        public string Mayor { get; set; }
+        public int Year { get; set; }
+
+        private List<Building> Buildings = new List<Building>();
+
+        public void AddBuilding(Building building)
+        {
+            Buildings.Add(building);
+        }
+
+
+
+
+    }
+}
